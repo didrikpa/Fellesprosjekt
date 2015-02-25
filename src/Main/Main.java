3a262f6ca1917be8f607b23c1fecfad09510fba2
MasoomMaham <masoommaham@gmail.com>
2015-02-25 13:52:21 +0100

    I did some stuff

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("monthViewPane.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Calender");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
