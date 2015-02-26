3447119eb801b0277c53db61993ff58076a0bbdd
MasoomMaham <masoommaham@gmail.com>
2015-02-26 11:02:54 +0100

    Added controller for monthview

---
+++ src/Controller/MonthViewControlller.java
@@ -0,0 +1,100 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+/**
+ * Created by masoom on 26.02.15.
+ */
+public class MonthViewControlller {
+
+    @FXML Pane pane1;
+    @FXML Pane pane2;
+    @FXML Pane pane3;
+    @FXML Pane pane4;
+    @FXML Pane pane5;
+    @FXML Pane pane6;
+    @FXML Pane pane7;
+    @FXML Pane pane8;
+    @FXML Pane pane9;
+    @FXML Pane pane10;
+    @FXML Pane pane11;
+    @FXML Pane pane12;
+    @FXML Pane pane13;
+    @FXML Pane pane14;
+    @FXML Pane pane15;
+    @FXML Pane pane16;
+    @FXML Pane pane17;
+    @FXML Pane pane18;
+    @FXML Pane pane19;
+    @FXML Pane pane20;
+    @FXML Pane pane21;
+    @FXML Pane pane22;
+    @FXML Pane pane23;
+    @FXML Pane pane24;
+    @FXML Pane pane25;
+    @FXML Pane pane26;
+    @FXML Pane pane27;
+    @FXML Pane pane28;
+    @FXML Pane pane29;
+    @FXML Pane pane30;
+    @FXML Pane pane31;
+    @FXML Pane pane32;
+    @FXML Pane pane33;
+    @FXML Pane pane34;
+    @FXML Pane pane35;
+    @FXML Pane pane36;
+    @FXML Pane pane37;
+    @FXML Pane pane38;
+    @FXML Pane pane39;
+    @FXML Pane pane40;
+    @FXML Pane pane41;
+    @FXML Pane pane42;
+
+    @FXML Label label1;
+    @FXML Label label2;
+    @FXML Label label3;
+    @FXML Label label4;
+    @FXML Label label5;
+    @FXML Label label6;
+    @FXML Label label7;
+    @FXML Label label8;
+    @FXML Label label9;
+    @FXML Label label10;
+    @FXML Label label11;
+    @FXML Label label12;
+    @FXML Label label13;
+    @FXML Label label14;
+    @FXML Label label15;
+    @FXML Label label16;
+    @FXML Label label17;
+    @FXML Label label18;
+    @FXML Label label19;
+    @FXML Label label20;
+    @FXML Label label21;
+    @FXML Label label22;
+    @FXML Label label23;
+    @FXML Label label24;
+    @FXML Label label25;
+    @FXML Label label26;
+    @FXML Label label27;
+    @FXML Label label28;
+    @FXML Label label29;
+    @FXML Label label30;
+    @FXML Label label31;
+    @FXML Label label32;
+    @FXML Label label33;
+    @FXML Label label34;
+    @FXML Label label35;
+    @FXML Label label36;
+    @FXML Label label37;
+    @FXML Label label38;
+    @FXML Label label39;
+    @FXML Label label40;
+    @FXML Label label41;
+    @FXML Label label42;
+    
+
+
+}
