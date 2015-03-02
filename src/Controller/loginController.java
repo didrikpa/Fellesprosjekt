6ce3e1dc498a33e735e05c66d35dd55332829a7e
MasoomMaham <masoommaham@gmail.com>
2015-03-02 15:56:12 +0100

    Chnaged the controller location for this fxml file

--- src/Controller/loginController.java
+++ src/Controller/loginController.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -26,9 +27,10 @@ public class loginController{
     		FXMLLoader loader = new FXMLLoader(getClass().getResource("monthViewPane.fxml"));
     		//Istedenfor å deklarere kontroller i fxml, så gjøres dette 
     		//"manuelt" for å videreføre DatabaseServer-objektet
-    		loader.setController(new MonthViewController(en));
+    		loader.setController(new MonthViewController());
+            Parent screen = loader.load();
     		stage = (Stage) loginPaneMain.getScene().getWindow();
-    		stage.setScene(new Scene(loader.load()));
+    		stage.setScene(new Scene(screen));
     		stage.show();
     	}
     	else{
@@ -36,11 +38,12 @@ public class loginController{
     		loginPanePassword.clear();
     	}
     }
-//    @FXML
-//    public void newUser(ActionEvent event) throws Exception{
-//    		FXMLLoader loader = new FXMLLoader(getClass().getResource("createUserPane.fxml"));
-//    		stage = (Stage) loginPaneMain.getScene().getWindow();
-//    		stage.setScene(new Scene(loader.load()));
-//    		stage.show();
-//    }
+    @FXML
+    public void newUser(ActionEvent event) throws Exception{
+    		FXMLLoader loader = new FXMLLoader(getClass().getResource("createUserPane.fxml"));
+    		stage = (Stage) loginPaneMain.getScene().getWindow();
+            Parent screen = loader.load();
+    		stage.setScene(new Scene(screen));
+    		stage.show();
+    }
 }
