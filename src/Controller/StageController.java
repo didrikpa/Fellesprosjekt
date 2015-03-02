87a84c6d5385cfc8283700b95c328459a011c0d0
MasoomMaham <masoommaham@gmail.com>
2015-03-02 12:21:57 +0100

    added stageController

---
+++ src/Controller/StageController.java
@@ -0,0 +1,40 @@
+package Controller;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StageController {
+    Stage stage = null;
+    Stage popUp = null;
+
+
+    public void setStage(String resource){
+        try{
+
+            FXMLLoader stageLoader = new FXMLLoader(getClass().getResource(resource));
+            Parent root = (Parent) stageLoader.load(); 
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e){
+            System.out.print(e);
+        }
+    }
+
+    public void setPopUp(String resource){
+        try{
+            FXMLLoader stageLoader = new FXMLLoader(getClass().getResource(resource));
+            Parent root = (Parent) stageLoader.load();
+            Scene scene = new Scene(root, 500, 300);
+            popUp.setScene(scene);
+            popUp.show();
+        }
+        catch (Exception e){
+            System.out.print(e);
+        }
+    }
+}
