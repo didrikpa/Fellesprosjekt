a26ac4b4ae4dd04aa08e4228f3f817a6ca12abdf
Ziphone <sschjeld@gmail.com>
2015-03-03 20:06:44 +0100

    Grunnleggende

--- src/Controller/mainMonthViewController.java
+++ src/Controller/mainMonthViewController.java
@@ -1,6 +1,28 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import Server.DatabaseServer;
+
 public class mainMonthViewController {
-  //Kan prøve å styre 4 views fra èn kontroller, hvorav kun tre views er synlige på en gang
-  
+	@FXML
+    Pane mainMonthViewPane;
+	@FXML
+    GridPane gridPane;
+	DatabaseServer dbSession;
+	Stage stage;
+	public mainMonthViewController(DatabaseServer server) throws Exception{
+		dbSession = server;
+		initScene();
+		stage = (Stage) mainMonthViewPane.getScene().getWindow();		
+	}
+	void initScene() throws Exception{
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("monthViewPane.fxml"));
+		fxmlLoader.setRoot(gridPane);
+		fxmlLoader.setController(new MonthViewController(dbSession));
+		fxmlLoader.load();
+	}
 }
