09ad4242b227b5bfa1d7fc07d4071cfe33ee2e29
Ziphone <sschjeld@gmail.com>
2015-03-03 09:13:56 +0100

    Forenkler createUser og annen bruk av brukeropplysninger

---
+++ src/Model/User.java
@@ -0,0 +1,53 @@
+package Model;
+public class User {
+	String username;
+	String password;
+	String firstname;
+	String lastname;
+	String email;
+	String phone;
+	public User(){
+		username = null;
+		password = null;
+		firstname = null;
+		lastname = null;
+		email = null;
+		phone = null;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
