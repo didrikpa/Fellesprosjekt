4556748738613f01ffcf4a808cbe5ed99d5ba0c1
MasoomMaham <masoommaham@gmail.com>
2015-03-04 12:37:48 +0100

    Added verification methods

--- src/Controller/createEventController.java
+++ src/Controller/createEventController.java
@@ -1,25 +1,39 @@
 package Controller;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.util.Callback;
 
+import java.net.URI;
+import java.net.URL;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class createEventController {
+public class createEventController implements Initializable {
 
     @FXML
     DatePicker createEventViewDatePicker;
     @FXML
-    ComboBox createEventViewHours;
+    ComboBox<Integer> createEventViewStartHours;
     @FXML
-    ComboBox createEventViewMinutes;
+    ComboBox<Integer> createEventViewStartMinutes;
     @FXML
-    ComboBox createEventViewRoom;
+    ComboBox<Integer> createEventViewEndHours;
+    @FXML
+    ComboBox<Integer> createEventViewEndMinutes;
+    @FXML
+    ComboBox<String> createEventViewRoom;
     @FXML
     TextField createEventViewSearch;
 
@@ -51,6 +65,89 @@ public class createEventController {
     }
 
 
+    @FXML
+    public void setHourFrom() {
+        List<Integer> someVariableName = new ArrayList();
+        for (int i = 0; i < 24; i++) someVariableName.add(i);
+        ObservableList<Integer> hours = FXCollections.observableArrayList(someVariableName);
+        createEventViewStartHours.setItems(hours);
+        createEventViewStartHours.setValue(LocalTime.now().getHour());
+    }
 
+    @FXML
+    public void setMinuteFrom() {
+        List<Integer> someVariableName = new ArrayList();
+        for (int i=0; i<60; i+=5) someVariableName.add(i);
+        ObservableList<Integer> minutes = FXCollections.observableArrayList(someVariableName);
+        createEventViewStartMinutes.setItems(minutes);
+        createEventViewStartMinutes.setValue(5 * (Math.round((LocalTime.now().getMinute() + 5) / 5)));
+        createEventViewStartHours.setValue(LocalTime.now().getHour() + 1);
+    }
+
+
+    @FXML
+    public void setHourToo(){
+        DecimalFormat formatter = new DecimalFormat("00");
+        int n = createEventViewStartHours.getValue();
+        List<Integer> hours = new ArrayList();
+        for (int i=n; i<24; i++) hours.add(i);
+        ObservableList<Integer> availableHours = FXCollections.observableArrayList(hours);
+        createEventViewEndHours.setItems(availableHours);
+        createEventViewEndHours.setValue(n + 1);
+    }
+
+    @FXML
+    public void setMinuteToo() {
+        int j = createEventViewStartMinutes.getValue();
+        List<Integer> someVariable = new ArrayList();
+        for (int i=0; i<60; i+=5) someVariable.add(i);
+        ObservableList<Integer> availableMinutes = FXCollections.observableArrayList(someVariable);
+        createEventViewEndMinutes.setItems(availableMinutes);
+        createEventViewEndMinutes.setValue(j);
+    }
+
+    @FXML
+    public boolean validateTime(){
+        if (createEventViewEndHours.getValue() == createEventViewStartHours.getValue() && createEventViewEndMinutes.getValue() < createEventViewStartMinutes.getValue()) {
+            createEventViewEndMinutes.setStyle("-fx-background-color: red");
+            //timeError.setText("Insert error");
+            //timeError.setVisible(true);
+            return false;
+        } else if (createEventViewStartHours.getValue() == createEventViewEndHours.getValue() && createEventViewStartMinutes.getValue() == createEventViewEndMinutes.getValue()) {
+            createEventViewEndMinutes.setStyle("-fx-background-color: red");
+            createEventViewEndHours.setStyle("-fx-background-color:  red");
+            //timeError.setVisible(true);
+            //timeError.setText("Insert error");
+            return false;
+        } else {
+            createEventViewEndMinutes.setStyle(" ");
+            createEventViewEndHours.setStyle(" ");
+            //timeError.setVisible(false);
+            return true;
+        }
+    }
+
+    @FXML
+    public boolean validateRoom(){
+        if (createEventViewRoom.getValue() == null){
+            //roomError.setVisible(true);
+            return false;
+        }
+        else {
+            //roomError.setVisible(false);
+            return true;
+        }
+    }
+
+    @FXML
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setDatePicker(createEventViewDatePicker);
+        setHourFrom();
+        setMinuteFrom();
+        setHourToo();
+        setMinuteToo();
+    }
 
 }
