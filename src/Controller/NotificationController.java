a1d6fe0396c6fe532af1b29088f60b705b48fa0d
MasoomMaham <masoommaham@gmail.com>
2015-03-06 16:21:09 +0100

    Add method that closes the window when the done button is pushed

--- src/Controller/NotificationController.java
+++ src/Controller/NotificationController.java
@@ -1,7 +1,17 @@
 package Controller;
 
-/**
- * Created by masoom on 06.03.15.
- */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class NotificationController {
+    Stage stage;
+
+    public void exitNotifications(ActionEvent e) throws Exception{
+        ((Node)(e.getSource())).getScene().getWindow().hide();
+    }
+
 }
