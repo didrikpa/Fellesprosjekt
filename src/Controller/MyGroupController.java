2a8c222b866d4f92d317fefbb0cd720154f76995
MasoomMaham <masoommaham@gmail.com>
2015-03-09 13:14:03 +0100

    Controller for group popup

---
+++ src/Controller/MyGroupController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import Server.*;
+import Model.*;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+/**
+ * Created by masoom on 09.03.15.
+ */
+public class MyGroupController implements Initializable {
+    @FXML
+    Label myGroups1;
+    @FXML
+    Label myGroups2;
+    List<String> myGroups;
+
+    DatabaseServer databaseServer = new DatabaseServer();
+    Group group;
+    public MyGroupController(DatabaseServer databaseServer){
+        this.databaseServer = databaseServer;
+    }
+
+    @FXML
+    public void getGroupNames(){
+        try {
+            myGroups = databaseServer.getGroups();
+        }
+        catch (Exception e) { System.out.println(e);}
+        myGroups1.setText(myGroups.get(0));
+        myGroups2.setText(myGroups.get(1));
+    }
+
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        try{
+
+            getGroupNames();
+        }
+        catch (Exception e) { System.out.println(e); }
+    }
+}
