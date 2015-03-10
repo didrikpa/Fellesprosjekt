a8b4b0e002ac20b772114c6ad53f93ba9bd07180
Ziphone <sschjeld@gmail.com>
2015-03-10 15:08:13 +0100

    changes for groupView

--- src/Server/DatabaseServer.java
+++ src/Server/DatabaseServer.java
@@ -19,7 +19,7 @@ public class DatabaseServer {
 	private String Password;
 	Connection conn;
 	Statement stmt;
-	
+
 	public DatabaseServer() {
 		try{
 			Class.forName("com.mysql.jdbc.Driver"); 
@@ -30,7 +30,7 @@ public class DatabaseServer {
 			System.out.println("Connection failed:" + e.getMessage());
 		}
 	}
-	
+
 	public boolean login(String B, String P) throws SQLException{
 		if(userExist(B)){
 			String values = "";
@@ -52,7 +52,7 @@ public class DatabaseServer {
 		}
 		return false;
 	}
-	
+
 	public User getUser() throws Exception {
 		User user = new User();
 		String sql = "SELECT * FROM Bruker WHERE Brukernavn = '" + this.Username + "';";
@@ -67,7 +67,7 @@ public class DatabaseServer {
 		}
 		return user;
 	}
-	
+
 	public ArrayList<User> getUsers() throws Exception {
 		ArrayList<User> users= new ArrayList<User>();
 		String sql = "SELECT * FROM Bruker;";
@@ -93,7 +93,7 @@ public class DatabaseServer {
 		}
 		return false;
 	}
-	
+
 	public void editUser(User user) throws Exception {
 		if(user.getUsername().equalsIgnoreCase(Username)){
 			String sql = "UPDATE Bruker SET Telefon ='" + user.getPhone() + "', Passord ='" + user.getPassword() + "' WHERE Brukernavn ='" + this.Username + "';";
@@ -119,7 +119,7 @@ public class DatabaseServer {
 			return true;
 		}
 	}
-	
+
 	public ArrayList<PersonalAppointment> getAppointment(Date date) throws Exception{
 		String sql = "SELECT * FROM Avtale, Bruker WHERE Bruker.Brukernavn = '" + Username + "' AND Avtale.Dato ='" + date.toString() + "' AND Bruker.Brukernavn = Avtale.Brukernavn;";
 		ResultSet rs = stmt.executeQuery(sql);
@@ -136,25 +136,28 @@ public class DatabaseServer {
 		}
 		return appointments;
 	}
-	
-	//Henter avtaler fra andre brukere
-	public ArrayList<PersonalAppointment> getAppointment(Date date, User user) throws Exception{
-		String sql = "SELECT * FROM Avtale, Bruker WHERE Bruker.Brukernavn = '" + user.getUsername() + "' AND Avtale.Dato ='" + date.toString() + "' AND Bruker.Brukernavn = Avtale.Brukernavn;";
-		ResultSet rs = stmt.executeQuery(sql);
+
+	public ArrayList<PersonalAppointment> getAppointment(Date date, Group group) throws Exception{
 		ArrayList <PersonalAppointment> appointments = new ArrayList<PersonalAppointment>();
-		while(rs.next()){
-			PersonalAppointment appointment = new PersonalAppointment();
-			appointment.setAvtaleID(Integer.parseInt(rs.getString("AvtaleID")));
-			appointment.setDato(Date.valueOf(rs.getString("Dato")));
-			appointment.setStartTid(Time.valueOf(rs.getString("Starttid")));
-			appointment.setSluttTid(Time.valueOf(rs.getString("Slutttid")));
-			appointment.setBeskrivelse(rs.getString("Beskrivelse"));
-			appointment.setRomnavn(rs.getString("Romnavn"));
-			appointments.add(appointment);
+		for(User user:group.getUsers()){
+			if(!user.getUsername().equals(Username)){
+				String sql = "SELECT * FROM Avtale, Bruker WHERE Bruker.Brukernavn = '" + user.getUsername() + "' AND Avtale.Dato ='" + date.toString() + "' AND Bruker.Brukernavn = Avtale.Brukernavn;";
+				ResultSet rs = stmt.executeQuery(sql);
+				while(rs.next()){
+					PersonalAppointment appointment = new PersonalAppointment();
+					appointment.setAvtaleID(Integer.parseInt(rs.getString("AvtaleID")));
+					appointment.setDato(Date.valueOf(rs.getString("Dato")));
+					appointment.setStartTid(Time.valueOf(rs.getString("Starttid")));
+					appointment.setSluttTid(Time.valueOf(rs.getString("Slutttid")));
+					appointment.setBeskrivelse(rs.getString("Beskrivelse"));
+					appointment.setRomnavn(rs.getString("Romnavn"));
+					appointments.add(appointment);
+				}
+			}
 		}
 		return appointments;
 	}
-	
+
 	//Henter n-antall nærmeste avtaler
 	public ArrayList<PersonalAppointment> comingUp(int n) throws Exception{
 		if(n > 0){
@@ -177,89 +180,88 @@ public class DatabaseServer {
 	}
 
 
-	
+
 	public void addAppointment(PersonalAppointment appointment) throws Exception {
 		String sql = "INSERT INTO Avtale VALUES ( NULL,'" + appointment.getDato().toString() + "', '" + appointment.getStartTid().toString() +"', '" + appointment.getSluttTid().toString() +"', '" + appointment.getBeskrivelse() +"', '" + appointment.getRomnavn() +"', '" + Username + "'," + null + ");";
 		stmt.executeUpdate(sql);
 	}
 
-    public boolean emailExist(String email) throws SQLException{
-        String values = "";
-        String sql = "SELECT `E-post` FROM Bruker WHERE `E-post` = '" + email + "';";
-        ResultSet rs = stmt.executeQuery(sql);
-        while(rs.next()){
-            values += (rs.getString("E-post"));
-        }
-        values.replaceAll(" ", "");
-        if(values.equals("")){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
-
-    public String getPassword(String email) throws Exception {
-        String password;
-        password = this.Password;
-        String sql = "SELECT Passord FROM Bruker WHERE `E-post` = '" + email + "' ;";
-        ResultSet rs = stmt.executeQuery(sql);
-        while(rs.next()){
-            password = rs.getString("Passord");
-        }
-        return password;
-    }
-
-    public List getRoomName() throws Exception {
-        List<String> romnavn = new ArrayList<String>();
-        String sql = "SELECT Romnavn FROM Møterom;";
-        ResultSet rs = stmt.executeQuery(sql);
-        while(rs.next()){
-            romnavn.add(rs.getString("Romnavn"));
-        }
-        return romnavn;
-    }
-
-	public ArrayList<Group> getGroups() throws Exception{
-        ArrayList<Group> group = new ArrayList<Group>();
-        String sql = "SELECT Gruppenavn FROM Gruppe, Gruppemedlem WHERE Gruppe.GruppeID = Gruppemedlem.GruppeID AND Gruppemedlem.Brukernavn = '" + Username + "';";
-        ResultSet rs = stmt.executeQuery(sql);
-        while (rs.next()){
-        	Group en = new Group();
-        	en.setGroupID(Integer.parseInt(rs.getString("GruppeID")));
-            en.setGroupName(rs.getString("Gruppenavn"));
-            en.setUsers(getUserGroup(en.getGroupID()));
-        }
-        return group;
-    }
-	
-	private ArrayList<User> getUserGroup(Integer gpid) throws Exception {
-		ArrayList<User>users = new ArrayList<User>();
-		String sql = "SELECT * FROM Bruker, Gruppe, Gruppemedlem WHERE Bruker.Brukernavn = Gruppemedlem.Brukernavn AND Gruppemedlem.GruppeID = Gruppe.GruppeID AND Gruppe.GruppeID = '" + gpid + "';";
+	public boolean emailExist(String email) throws SQLException{
+		String values = "";
+		String sql = "SELECT `E-post` FROM Bruker WHERE `E-post` = '" + email + "';";
 		ResultSet rs = stmt.executeQuery(sql);
 		while(rs.next()){
-			User user = new User();
-			user.setUsername(rs.getString("Brukernavn"));
-			user.setPassword(rs.getString("Passord"));
-			user.setFirstname(rs.getString("Fornavn"));
-			user.setLastname(rs.getString("Etternavn"));
-			user.setEmail(rs.getString("E-post"));
-			user.setPhone(rs.getString("Telefon"));
-			users.add(user);
+			values += (rs.getString("E-post"));
 		}
-		return users;
+		values.replaceAll(" ", "");
+		if(values.equals("")){
+			return false;
+		}
+		else{
+			return true;
+		}
 	}
 
-    public List getGroupMembers(String groupName) throws Exception{
-        List<String> groupMembers = new ArrayList<String>();
-        String sql = "SELECT Fornavn, Etternavn FROM Gruppe, Gruppemedlem, Bruker WHERE Gruppe.GruppeID = Gruppemedlem.GruppeID AND Gruppemedlem.Brukernavn = Bruker.BrukerNavn AND Gruppe.GruppeID = '" + groupName + "';";
-        ResultSet rs = stmt.executeQuery(sql);
-        while (rs.next()){
-            groupMembers.add(rs.getString("Fornavn"));
-            groupMembers.add(rs.getString("Etternavn"));
-        }
-        return groupMembers;
-    }
+	public String getPassword(String email) throws Exception {
+		String password;
+		password = this.Password;
+		String sql = "SELECT Passord FROM Bruker WHERE `E-post` = '" + email + "' ;";
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()){
+			password = rs.getString("Passord");
+		}
+		return password;
+	}
+
+	public List getRoomName() throws Exception {
+		List<String> romnavn = new ArrayList<String>();
+		String sql = "SELECT Romnavn FROM Møterom;";
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()){
+			romnavn.add(rs.getString("Romnavn"));
+		}
+		return romnavn;
+	}
+
+	public ArrayList<Group> getGroups() throws Exception{
+		ArrayList<Group> group = new ArrayList<Group>();
+		String sql = "SELECT Gruppe.Gruppenavn, Gruppe.GruppeID FROM Gruppe, Gruppemedlem WHERE Gruppe.GruppeID = Gruppemedlem.GruppeID AND Gruppemedlem.Brukernavn = '" + Username + "';";
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()){
+			Group en = new Group();
+			en.setGroupID(Integer.parseInt(rs.getString("GruppeID")));
+			en.setGroupName(rs.getString("Gruppenavn"));
+			group.add(en);
+		}
+		for(Group gp:group){
+			ArrayList<User>users = new ArrayList<User>();
+			sql = "SELECT * FROM Bruker, Gruppe, Gruppemedlem WHERE Bruker.Brukernavn = Gruppemedlem.Brukernavn AND Gruppemedlem.GruppeID = Gruppe.GruppeID AND Gruppe.GruppeID = '" + gp.getGroupID() + "';";
+			rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				User user = new User();
+				user.setUsername(rs.getString("Brukernavn"));
+				user.setPassword(rs.getString("Passord"));
+				user.setFirstname(rs.getString("Fornavn"));
+				user.setLastname(rs.getString("Etternavn"));
+				user.setEmail(rs.getString("E-post"));
+				user.setPhone(rs.getString("Telefon"));
+				users.add(user);
+			}
+			gp.setUsers(users);
+		}
+		return group;
+	}
+
+	public List getGroupMembers(String groupName) throws Exception{
+		List<String> groupMembers = new ArrayList<String>();
+		String sql = "SELECT Fornavn, Etternavn FROM Gruppe, Gruppemedlem, Bruker WHERE Gruppe.GruppeID = Gruppemedlem.GruppeID AND Gruppemedlem.Brukernavn = Bruker.BrukerNavn AND Gruppe.GruppeID = '" + groupName + "';";
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()){
+			groupMembers.add(rs.getString("Fornavn"));
+			groupMembers.add(rs.getString("Etternavn"));
+		}
+		return groupMembers;
+	}
 
 
 	public void quit() throws SQLException{
@@ -267,31 +269,31 @@ public class DatabaseServer {
 		stmt.close();
 	}
 
-    public void hashPassword(String Password)
-    {
-        String passwordToHash = Password;
-        String generatedPassword = null;
-        try {
-            // Create MessageDigest instance for MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            //Add password bytes to digest
-            md.update(passwordToHash.getBytes());
-            //Get the hash's bytes
-            byte[] bytes = md.digest();
-            //This bytes[] has bytes in decimal format;
-            //Convert it to hexadecimal format
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            //Get complete hashed password in hex format
-            generatedPassword = sb.toString();
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            e.printStackTrace();
-        }
-        System.out.println(generatedPassword);
-    }
+	public void hashPassword(String Password)
+	{
+		String passwordToHash = Password;
+		String generatedPassword = null;
+		try {
+			// Create MessageDigest instance for MD5
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			//Add password bytes to digest
+			md.update(passwordToHash.getBytes());
+			//Get the hash's bytes
+			byte[] bytes = md.digest();
+			//This bytes[] has bytes in decimal format;
+			//Convert it to hexadecimal format
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i< bytes.length ;i++)
+			{
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			//Get complete hashed password in hex format
+			generatedPassword = sb.toString();
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println(generatedPassword);
+	}
 }
