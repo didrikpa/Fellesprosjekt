77a6adcc9a8a99828aec6228ec97577ff3c0bcf4
Ziphone <sschjeld@gmail.com>
2015-03-10 07:44:41 +0100

    småfiks + endring av navn

--- src/Controller/AppointmentController.java
+++ src/Controller/AppointmentController.java
@@ -1,26 +1,34 @@
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import Model.PersonalAppointment;
 import Server.DatabaseServer;
 
-public class appointmentController implements Initializable{
+public class AppointmentController implements Initializable{
 	@FXML Label apDate;
 	@FXML Label apRoom;
 	@FXML Label apTime;
 	@FXML Label apDescription;
+	CalendarViewController cvc;
 	DatabaseServer dbserver;
+	Stage stage;
 	PersonalAppointment pa;
-	public appointmentController(DatabaseServer server, PersonalAppointment psa){
+	public AppointmentController(DatabaseServer server, PersonalAppointment psa, CalendarViewController cc){
 		dbserver = server;
 		pa = psa;
+		cvc = cc;
 		init();
 		initialize(null, null);
 	}
@@ -29,6 +37,7 @@ public class appointmentController implements Initializable{
 		apRoom = new Label();
 		apTime = new Label();
 		apDescription = new Label();
+		stage = null;
 	}
 	
 	@FXML 
@@ -37,7 +46,13 @@ public class appointmentController implements Initializable{
 	}
 	
 	@FXML
-	public void editAppointment(ActionEvent event){
+	public void editAppointment(ActionEvent event) throws IOException{
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/EditEventController.fxml"));
+		fxmlLoader.setController(new EditEventController(dbserver, pa, cvc));
+		stage = new Stage();
+		stage.setTitle("Appointment - ID." + pa.getAvtaleID());
+		stage.setScene(new Scene((Parent) fxmlLoader.load()));
+		stage.show();
 		((Node)(event.getSource())).getScene().getWindow().hide();
 	}
 	
