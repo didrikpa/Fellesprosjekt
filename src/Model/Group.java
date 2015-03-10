742a94e9b45067cd014331153bb07795e5c031e9
Ziphone <sschjeld@gmail.com>
2015-03-10 14:15:35 +0100

    Update Group.java

--- src/Model/Group.java
+++ src/Model/Group.java
@@ -2,25 +2,33 @@ package Model;
 
 import java.util.ArrayList;
 
-/**
- * Created by henrikmm on 3/3/15.
- */
 // Klasse for grupper som brukere er en del av.
 public class Group {
     private Integer groupID;
     private ArrayList<User> users;
+    private String groupName;
 
 //  Instanserer brukerlisten i gruppa.
-    public Group(){users = new ArrayList<User>();}
+    public Group(){
+    	users = new ArrayList<User>();
+    	groupName = null;
+    	
+    	}
 
 //    Returnerer alle brukere i gruppen.
-    public ArrayList<User> getUsers() {return users;}
+    public ArrayList<User> getUsers() {
+    	return users;
+    	}
 
 //  Setter alle brukere i gruppen med argument som en ArrayList<User>.
-    public void setUsers(ArrayList<User> users) {this.users = users;}
+    public void setUsers(ArrayList<User> users) {
+    	this.users = users;
+    	}
 
 //  Legger til en bruker i gruppen.
-    public void addUser(User user){users.add(user);}
+    public void addUser(User user){
+    	users.add(user);
+    	}
 
 //  Hvis brukeren er en del av gruppen, returnerer denne brukeren.
     public User getUser(User user){
@@ -35,10 +43,31 @@ public class Group {
     }
 
 //  Returnerer gruppeIDen.
-    public Integer getGroupID() {return groupID;}
+    public Integer getGroupID() {
+    	return groupID;
+    	}
 
 
 //  Setter gruppeIDen som et Integer.
-    public void setGroupID(Integer groupID) {this.groupID = groupID;}
+    public void setGroupID(Integer groupID) {
+    	this.groupID = groupID;
+    	}
+    
+    public String getGroupName() {
+    	return this.groupName;
+    	}
 
+
+    public void setGroupName(String gn) {
+    	this.groupName = gn;
+    	}
+    @Override
+    public String toString() {
+    	String mid = "";
+    	for(User user : users){
+    		mid += user.firstname + " " + user.lastname + ", " ;
+    	}
+    	mid = mid.substring(0, mid.length()-2);
+    	return groupName + mid;
+    }
 }
