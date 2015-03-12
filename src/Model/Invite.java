9049f4b7767f4cb3f0d0ba9cefe5630a05d46b39
Ziphone <sschjeld@gmail.com>
2015-03-12 17:06:28 +0100

    Update Invite.java

--- src/Model/Invite.java
+++ src/Model/Invite.java
@@ -1,50 +1,96 @@
-package Model;
+package Controller;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import Model.Alarm;
+import Model.Invite;
+import Model.Påminnelse;
 import Server.DatabaseServer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
-public class Invite {
-	int InvitasjonsID;
-	String Brukernavn;
-	int AvtaleID;
-	boolean Godtatt;
+public class NotificationController implements Initializable{
+	Stage stage;
+	@FXML ListView<Påminnelse> notificationList;
+	ArrayList<Påminnelse> pas;
+	@FXML Button notificationsAccept;
+	@FXML Button notifictionsDecline;
+	CalendarViewController parent;
 	DatabaseServer server;
-	public Invite(DatabaseServer dbserver){
+	public NotificationController(DatabaseServer dbserver, CalendarViewController par){
+		parent = par;
 		server = dbserver;
+		pas = new ArrayList<Påminnelse>();
+		notificationList = new ListView<Påminnelse>();
+		initialize(null, null);
 	}
-	
-	public int getInvitasjonsID() {
-		return InvitasjonsID;
+	@FXML
+	public void exitNotifications(ActionEvent e) throws Exception{
+		((Node)(e.getSource())).getScene().getWindow().hide();
 	}
-	public void setInvitasjonsID(int invitasjonsID) {
-		InvitasjonsID = invitasjonsID;
+
+	@FXML
+	public void notificationsAccept(ActionEvent event) throws Exception {
+		ObservableList<Påminnelse> invitasjoner = notificationList.getSelectionModel().getSelectedItems();
+		if(invitasjoner.get(0) instanceof Invite){
+			Invite inv = (Invite)invitasjoner.get(0);
+			server.respond(inv,true);
+			loadList();
+			notificationList.setItems(FXCollections.observableArrayList(pas));
+			parent.monthB();
+			parent.monthF();
+		}
+		if(invitasjoner.get(0) instanceof Alarm){
+			Alarm larm = (Alarm)invitasjoner.get(0);
+			server.removeAlarm(larm);
+			loadList();
+			notificationList.setItems(FXCollections.observableArrayList(pas));
+		}
 	}
-	public String getBrukernavn() {
-		return Brukernavn;
+
+	@FXML
+	public void notificationsDecline(ActionEvent event) throws Exception {
+		ObservableList<Påminnelse> invitasjoner = notificationList.getSelectionModel().getSelectedItems();
+		if(invitasjoner.get(0) instanceof Invite){
+			Invite inv = (Invite)invitasjoner.get(0);
+			server.respond(inv,false);
+			loadList();
+			notificationList.setItems(FXCollections.observableArrayList(pas));
+			parent.monthB();
+			parent.monthF();
+		}
+		if(invitasjoner.get(0) instanceof Alarm){
+			Alarm larm = (Alarm)invitasjoner.get(0);
+			server.removeAlarm(larm);
+			loadList();
+			notificationList.setItems(FXCollections.observableArrayList(pas));
+		}
+
 	}
-	public void setBrukernavn(String brukernavn) {
-		Brukernavn = brukernavn;
+	private void loadList() throws Exception {
+		pas.clear();
+		for(Invite ene : server.getInvites()){
+			pas.add(ene);
+		}
+		for(Alarm tone : server.getAlarm()){
+			pas.add(tone);
+		}
 	}
-	public int getAvtaleID() {
-		return AvtaleID;
-	}
-	public void setAvtaleID(int avtaleID) {
-		AvtaleID = avtaleID;
-	}
-	public boolean isGodtatt() {
-		return Godtatt;
-	}
-	public void setGodtatt(boolean godtatt) {
-		Godtatt = godtatt;
-	}
-	@Override
-	public String toString() {
-		PersonalAppointment pa = null;
+	public void initialize(URL url, ResourceBundle resourceBundle) {
 		try {
-			pa = server.specificAppointment(getAvtaleID());
+			loadList();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return AvtaleID + " - \" " + pa.getBeskrivelse() + "\" invitert av " + pa.getOpprettetAv();
+		notificationList.setItems(FXCollections.observableArrayList(pas));
 	}
-
 }
