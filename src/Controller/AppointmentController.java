7b7974237998d1b4648b9db6780b2dbe39c80bb9
Nomis <sschjeld@gmail.com>
2015-03-13 12:07:48 +0100

    Update AppointmentController.java

--- src/Controller/AppointmentController.java
+++ src/Controller/AppointmentController.java
@@ -2,10 +2,13 @@ package Controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import com.mysql.fabric.Server;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,17 +17,27 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.stage.Stage;
+import Model.Alarm;
 import Model.PersonalAppointment;
 import Server.DatabaseServer;
 
 public class AppointmentController implements Initializable{
 	@FXML Button changeAppointment;
+	@FXML Button createAlarm;
+	@FXML Button deleteAppointment;
 	@FXML Label apDate;
 	@FXML Label apRoom;
 	@FXML Label apTime;
 	@FXML Label apDescription;
+	@FXML TextField alarmIn;
+	@FXML ComboBox<String> alarmChoice;
+	@FXML ToggleButton toogleGoing;
+	@FXML ToggleButton toogleNotGoing;
 	CalendarViewController cvc;
 	DatabaseServer dbserver;
 	Stage stage;
@@ -37,17 +50,32 @@ public class AppointmentController implements Initializable{
 		initialize(null, null);
 	}
 	void init(){
+		createAlarm = new Button();
+		deleteAppointment = new Button();
 		changeAppointment = new Button();
+		alarmChoice = new ComboBox<String>();
 		apDate = new Label();
 		apRoom = new Label();
 		apTime = new Label();
 		apDescription = new Label();
 		stage = null;
+		toogleGoing = new ToggleButton();
+		toogleNotGoing = new ToggleButton();
 	}
 	
 	@FXML 
 	 public void cancelAppointment(ActionEvent event){
 		((Node)(event.getSource())).getScene().getWindow().hide();
+	}
+	
+	@FXML
+	public void toogleGoing(ActionEvent event) throws IOException{
+		
+	}
+	
+	@FXML
+	public void toogleNotGoing(ActionEvent event) throws IOException{
+		
 	}
 	
 	@FXML
@@ -60,9 +88,68 @@ public class AppointmentController implements Initializable{
 		stage.show();
 		((Node)(event.getSource())).getScene().getWindow().hide();
 	}
+	@FXML
+	public void createAlarm(ActionEvent event) {
+		Alarm alarm = new Alarm(dbserver);
+		Calendar calendar = Calendar.getInstance();
+		java.sql.Timestamp ts = new java.sql.Timestamp(calendar.getTime().getTime());
+		try {
+			int not = Integer.parseInt(alarmIn.getText());
+			if(not == 0){
+				alarm.setTidspunkt(null);
+			}
+			if(((String)alarmChoice.getValue()).equalsIgnoreCase("minutes")){
+				ts.setMinutes(ts.getMinutes()+not);
+				alarm.setTidspunkt(ts);
+			}
+			if(((String)alarmChoice.getValue()).equalsIgnoreCase("hours")){
+				ts.setHours(ts.getHours()+not);
+				alarm.setTidspunkt(ts);
+			}
+			if(((String)alarmChoice.getValue()).equalsIgnoreCase("days")){
+				ts.setDate(ts.getDay()+not);
+				alarm.setTidspunkt(ts);
+			}
+			if(alarm.getTidspunkt()!= null){
+				alarm.setAvtaleID(pa.getAvtaleID());
+				alarm.setBrukernavn(dbserver.Username);
+				dbserver.setAlarm(alarm);
+				alarmChoice.setVisible(false);
+				alarmIn.setVisible(false);
+				createAlarm.setVisible(false);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	@FXML
+	public void deleteAppointment(ActionEvent event) throws Exception {
+		dbserver.removeAppointment(pa);
+	}
+	
+	private void setNotifyComboValues() {
+		List<String> list = new ArrayList<String>();
+		list.add("minutes");
+		list.add("hours");
+		list.add("days");
+		ObservableList<String> choices = FXCollections
+				.observableArrayList(list);
+		alarmChoice.setItems(choices);
+		alarmChoice.setValue(choices.get(0));
+	}
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		setNotifyComboValues();
+		try {
+			if(!dbserver.isChildEvent(pa)){
+				toogleGoing.setVisible(false);
+				toogleNotGoing.setVisible(false);
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 		try {
 			if(cvc.groupCal != null || !pa.getOpprettetAv().equalsIgnoreCase(dbserver.Username) || dbserver.isChildEvent(pa)){
 				changeAppointment.setVisible(false);
