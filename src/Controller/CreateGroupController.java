d1fc855abe238daf07a4f10b69841ff16c70c285
Nomis <sschjeld@gmail.com>
2015-03-17 12:25:45 +0100

    Update CreateGroupController.java

--- src/Controller/CreateGroupController.java
+++ src/Controller/CreateGroupController.java
@@ -1,14 +1,17 @@
 package Controller;
 
+import Model.User;
 import Server.DatabaseServer;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class CreateGroupController {
 
@@ -23,15 +26,20 @@ public class CreateGroupController {
 
     @FXML
     Label groupNameLabel;
-
+    
+    CreateEventController cec;
     DatabaseServer server = new DatabaseServer();
-
-    public CreateGroupController(DatabaseServer server){
+    ArrayList<User>newGroup;
+    public CreateGroupController(DatabaseServer server, ArrayList<User>nG, CreateEventController ce){
         this.server = server;
+        cec = ce;
+        newGroup = nG;
+        groupNameLabel = new Label();
     }
-
+    
+    @FXML
     public void setCreateGroupButton(ActionEvent event) throws Exception{
-        List groupList = server.getAllGroups();
+        ArrayList<String> groupList = server.getAllGroups();
         if(groupNameField.getText().isEmpty()){
             groupNameLabel.setText("This field can't be empty");
             groupNameLabel.setVisible(true);
@@ -49,14 +57,18 @@ public class CreateGroupController {
 
     public void createGroup(String groupname){
         try {
-            server.createGroup(groupname);
+        	server.createGroup(groupname, newGroup);
+            cec.setGroups();
+            cec.createEventViewGroup.setValue(groupname);
+            cec.participantList.setItems(FXCollections.observableArrayList(cec.selectedUsers));
         }
         catch (Exception e)
         {
             System.out.println(e);
         }
     }
-
+    
+    @FXML
     public void setCancelButton(ActionEvent event){
         ((Node) (event.getSource())).getScene().getWindow().hide();
     }
