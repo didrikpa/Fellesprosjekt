413413bc6af83b722f088a0148b9abb3672f70a9
Henrik Mørk <mn.henrik@gmail.com>
2015-03-18 13:41:58 +0100

    Create tests.

--- src/Tests/TimeEventTrue.java
+++ src/Tests/TimeEventTrue.java
@@ -6,24 +6,44 @@ package Tests;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import Server.DatabaseServer;
+import Controller.CalendarViewController;
+import Model.User;
 
 
 import org.junit.Test;
 import org.loadui.testfx.GuiTest;
 
 public class TimeEventTrue extends GuiTest{
+    DatabaseServer server;
+    CalendarViewController controller;
+    User user;
+    public TimeEventTrue(){
+        user = new User();
+        user.setUsername("henrikmm");
+        user.setPassword("testtest");
+        server = new DatabaseServer();
+        try{
+            server.login("henrikmm", "testtest");}
+        catch (SQLException e){
+            System.out.println(e);
+        }
+        try{
+        controller = new CalendarViewController(server);}
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
     @Override
     protected Parent getRootNode() {
         try{
-            return FXMLLoader.load(this.getClass().getResource("/Views/loginView.fxml"));
+            return FXMLLoader.load(this.getClass().getResource("/Views/createEventView.fxml"));
         }catch (IOException e){
             System.err.println(e);
 
@@ -33,18 +53,20 @@ public class TimeEventTrue extends GuiTest{
 
     @Test
     public void testTrueEventTime(){
-        TextField usernameField = find("#loginPaneUsername");
-        String text1 = "henrikmm";
-        clickOn(usernameField).type(text1);
-        PasswordField passwordField = find("#loginPanePassword");
-        String text2 = "testtest";
-        clickOn(passwordField).type(text2);
-        clickOn("#loginPaneLoginButton");
+        DatePicker date = find("#createEventViewDatePicker");
+        String date2 = "3/20/2015";
+        clickOn(date).type(date2);
+        Object startHour = ((ComboBox) find("#createEventViewStartHours")).getValue();
+        Object startMin = ((ComboBox) find("#createEventViewStartMinutes")).getValue();
+        Object endHour = ((ComboBox) find("#createEventViewEndHours")).getValue();
+        Object endMin = ((ComboBox) find("#createEventViewEndMinutes")).getValue();
 
-        clickOn("#createEventCalendarView");
-        clickOn("#createEventViewDatePicker");
+        TextArea desc = find("#createEventViewTextArea");
+        String descr = "TestDescription.";
+        clickOn(desc).type(descr);
 
 
-
+        clickOn("#createEventViewCreate");
     }
+
 }
